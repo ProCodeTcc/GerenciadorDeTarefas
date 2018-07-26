8e832851d655add25d152db6706f9ce3f0cd7476
ProCodeTcc <41592056+ProCodeTcc@users.noreply.github.com>
2018-07-26 16:37:25 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+    function conectar(){
+        $conexao = mysqli_connect('10.107.144.27', 'root', 'bcd127','dbtarefa');
+        mysqli_set_charset($conexao,"utf8");
+        
+        if($conexao){
+            return $conexao;
+        }else{
+            echo("<script> alert('Erro ao conectar com o Banco de Dados') </script>");
+        }
+    }
+?>
